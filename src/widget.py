2d43b779b96c0578fe1a1263f7e9f0d0b986033b
Aleksey Bronnikov <lesha.bronnikov.99@mail.ru>
2025-04-28 17:06:47 +0300

    добавил в модуль widget.py функцию get_date

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,11 @@
-# Аргументом может быть строка типа
-# Visa Platinum 7000792289606361
-# Maestro 7000792289606361
-# Счет 73654108430135874305
-# Возвращать строку с замаскированным номером.
-# Для карт и счетов используйте разные типы маскировки.
-# Переиспользуйте уже существующие функции маскировки из вашего проекта,
-# чтобы избежать дублирования кода.
-
-               # Пример для карты
-# Visa Platinum 7000792289606361  # входной аргумент
-# Visa Platinum 7000 79** **** 6361  # выход функции
-               # Пример для счета
-# Счет 73654108430135874305  # входной аргумент
-# Счет **4305  # выход функции
+from typing import Union
 
 
-def mask_account_card(account_card):
-    """Функция, принимающая карту или счет с их названиями и возвращает соответствующую маску"""
+def mask_account_card(account_card: Union[str]) -> str:
+    """Функция, принимающая карту или счет с их названиями
+    и возвращает соответствующую маску"""
     list1 = []
-    card_string = ''
+    card_string = ""
     for symbol in account_card:
         if symbol.isdigit():
             card_string += symbol
@@ -28,21 +15,36 @@ def mask_account_card(account_card):
 
         index_start = 0
         index_end = 4
-        for _ in card_mask_account[:: 4]:
+        for _ in card_mask_account[::4]:
             list1.append(card_mask_account[index_start:index_end])
             index_start += 4
             index_end += 4
 
-        total_result = ' '.join(list1)
+        total_result = " ".join(list1)
         return account_card[0:-16] + total_result
 
     elif len(card_string) == 20:
-        mask_account =  "*" * (len(card_string) - 18) + card_string[-4:]
+        mask_account = "*" * (len(card_string) - 18) + card_string[-4:]
 
         return mask_account
+    return "Неверно введены данные"
 
 
-user_input_card = 'Maestro 7000792289606361'
+user_input_card = "Maestro 7000792289606361"
 
 result_mask_card = mask_account_card(user_input_card)
 print(result_mask_card)
+
+
+def get_date(current_date: Union[str]) -> str:
+    """Функция, принимающая дату и возвращающая её в нужном формате"""
+    repl_1 = current_date.replace("-", ".")
+    repl_2 = repl_1.replace("-", ".")
+    take_date = repl_2[:10]
+    our_date = ".".join(reversed(take_date.split(".")))
+    return our_date
+
+
+user_input_date = "2024-03-11T02:26:18.671407"
+result_get_date = get_date(user_input_date)
+print(result_get_date)
