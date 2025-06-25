18033e03bff20082c87ddcba59effcf15d22b147
Aleksey Bronnikov <lesha.bronnikov.99@mail.ru>
2025-06-25 20:44:24 +0300

    Реализовал ф-цию def reading_file_csv

--- src/reading_csv_xlsx_files.py
+++ src/reading_csv_xlsx_files.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import csv
 import json
+from pathlib import Path
 
 
 def reading_file_csv(path_csv):
+    file_path = path_csv / "transactions.csv"
+    absolute_path = file_path.resolve()
     try:
-        with open(path_csv, "r", encoding="utf-8") as file_csv:
-            read_csv = csv.DictReader(file_csv, delimiter=";")
-            for row in read_csv:
-                print(f"Операцию произвел: {row['from']}. Средства получил: {row['to']}. Действие: {row['description']}")
+        df = pd.read_csv(absolute_path, sep=";")
+        js_dict = df.to_dict(orient="records")
+        return json.dumps(js_dict, ensure_ascii=False, indent=4)
 
     except FileNotFoundError as file:
         print(f"Файл {file} не найден")
@@ -16,6 +18,8 @@ def reading_file_csv(path_csv):
 
 
 
+
 if __name__ == "__main__":
-    result_read_csv = reading_file_csv("transactions.csv")
+    home_dir = Path.cwd()
+    result_read_csv = reading_file_csv(home_dir)
     print(result_read_csv)
